ca6f2e48d8a83cbb4689aecb99de3e8447224e95
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2019-03-03 16:07:20 +0200

    initial for tests

--- Selenium/UnitTest1.cs
+++ Selenium/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Xunit;
 
@@ -7,16 +8,32 @@ namespace Selenium
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GoldenTest()
         {
             var driver = new ChromeDriver("./");
 
+            // check home page has products
+            driver.Navigate().GoToUrl("https://kuberton.mac-blog.org.ua/");
+            Assert.True(driver.FindElementsByCssSelector(".card").Count > 0, "products on home page");
+
+            // go to first product
+            driver.FindElementsByCssSelector(".card")[0].FindElement(By.CssSelector("a")).Click();
+
+            // add it to cart
+            driver.FindElementByCssSelector("button").Click();
+
             driver.Navigate().GoToUrl("https://kuberton.mac-blog.org.ua/");
 
-            var btn = driver.FindElementByCssSelector("button");
+            // go to second product
+            driver.FindElementsByCssSelector(".card")[1].FindElement(By.CssSelector("a")).Click();
 
-            Assert.Null(btn);
-            // OpenQA.Selenium.DriverServiceNotFoundException : The chromedriver file does not exist in the current directory or in a directory on the PATH environment variable. The driver can be downloaded at http://chromedriver.storage.googleapis.com/index.html.
+            // add it to cart
+            driver.FindElementByCssSelector("button").Click();
+
+            // TODO: assert cart
+            // TODO: checkout
+            // TODO: assert ok
+
 
             driver.Quit();
         }
